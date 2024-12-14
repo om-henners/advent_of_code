c5a65a624eab4353f0bcd7abe7d5ce568db95a43
Henry Walshaw <henry@floodmapp.com>
2024-12-14 22:10:40 +1100

    Day 13 - finding roots with sympy

---
+++ 2024/day_13/solution_1.py
@@ -0,0 +1,34 @@
+from sympy import solve, Integer
+from sympy.abc import a, b
+import textfsm
+
+
+template = textfsm.TextFSM(open("template"))
+machines = template.ParseText(open("input").read())
+
+
+def is_whole_token(root: dict) -> bool:
+    return isinstance(root[a], Integer) and isinstance(root[b], Integer)
+
+
+def cost(root: dict) -> Integer:
+    return root[a] * 3 + root[b]
+
+
+costs = []
+
+for machine in machines:
+    xs, ys, end_x, end_y = machine
+
+    eq_x = a * int(xs[0]) + b * int(xs[1]) - int(end_x)
+    eq_y = a * int(ys[0]) + b * int(ys[1]) - int(end_y)
+
+    roots = solve([eq_x, eq_y], a, b, dict=True)
+    roots = filter(is_whole_token, roots)
+
+    try:
+        costs.append(cost(min(roots, key=cost)))
+    except ValueError:
+        pass
+
+print(sum(costs))
